dce45ddca1eabfec9299b8360429dc4f96574eb5
Crowdbotics <team@crowdbotics.com>
2020-09-02 09:24:34 +0000

    Update models

--- backend/uncommon_sense_19927/wsgi.py
+++ backend/uncommon_sense_19927/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uncommon_sense_19927.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uncommon_sense_19927.settings")
 
 application = get_wsgi_application()
